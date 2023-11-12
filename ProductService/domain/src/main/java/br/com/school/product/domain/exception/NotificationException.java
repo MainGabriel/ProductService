3a52be23ded9d424bc173e7681cbe8b8078ea2f6
Gabriel Souza <dev_gabrielsouza@hotmail.com>
2023-11-12 16:03:02 -0300

    add project spring+kafka+test

---
+++ ProductService/domain/src/main/java/br/com/school/product/domain/exception/NotificationException.java
@@ -0,0 +1,10 @@
+package br.com.school.product.domain.exception;
+
+import br.com.school.product.domain.validation.NotificationValidation;
+
+public class NotificationException extends DomainException{
+
+    public NotificationException(final String message, final NotificationValidation notificationValidation) {
+        super(message, notificationValidation.getErrors());
+    }
+}
